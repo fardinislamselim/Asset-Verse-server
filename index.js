6658d976ecf2e88ed0677a831ce751f06df99289
fardinislamselim <codewithfardin@gmail.com>
2025-12-18 01:46:46 +0600

    fix: remove trailing slashes from CORS origin URLs

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173/", "https://assets-verse.web.app/"],
+    origin: ["http://localhost:5173", "https://assets-verse.web.app"],
     credentials: true,
   })
 );
